4425f0ab4abf203923e11d26b3ca4f6323505c0e
Ron Daryl Magno <ron.zohan@gmail.com>
2017-12-11 00:13:38 +0800

    Initial commit

---
+++ Microfeature/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Microfeature
+//
+//  Created by Ron Daryl Magno on 12/10/17.
+//  Copyright © 2017 Ron Daryl Magno. All rights reserved.
+//
+
+import UIKit
+import ComponentFeature
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
